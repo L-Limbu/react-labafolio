2e8f03d6913173f174e2d7801cb725ade268d3cd
L-Limbu <007limbu@gmail.com>
2021-11-04 18:11:29 +0000

    making the text wavy

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='waveAnimationSlow'>
           Welcome to my portfolio website. Hope you enjoy my work.
         </p>
+
       </header>
     </div>
   );
