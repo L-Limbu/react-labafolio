d819dbc428dda6c846508d7a67d9acc990e3a1c2
L-Limbu <007limbu@gmail.com>
2021-11-08 12:26:47 +0000

    box height now constant

--- src/pages/WorkPage.js
+++ src/pages/WorkPage.js
@@ -15,26 +15,34 @@ export default function WorkPage () {
 
                     <div className='boxes' onClick={enter()}>
                         <h2 className='waveAnimationSlow'>Looking at the solar system</h2>
-                        <i className="fas fa-meteor" ></i>
-                        <p> </p>
-                        <button >Know more</button>
+                        <div className='box-text'>
+                            <i className="fas fa-meteor" ></i>
+                            <p> Some text about the solar system</p>
+                            <button >Know more</button>
+                        </div>
                     </div>
                     <div className='boxes'>
                         <h2 className='waveAnimationSlow'>Trapping an atom</h2>
-                        <i className="fas fa-atom"  ></i>
-                        <p> </p>
-                        <button >Know more</button>
+                        <div className='box-text'>
+                            <i className="fas fa-atom"  ></i>
+                            <p> Some text about trapping an atom</p>
+                            <button >Know more</button>
+                        </div>
                     </div>
                     <div className='boxes'>
                         <h2 className='waveAnimationSlow'>Weird Magnet</h2>
-                        <i className="fas fa-magnet" ></i>
-                        <p> </p>
-                        <button >Know more</button>
+                        <div className='box-text'>
+                            <i className="fas fa-magnet" ></i>
+                            <p> Some text about magnets</p>
+                            <button >Know more</button>
+                        </div>
                     </div>
-                    <div className='boxes' onMouseEnter={enter()}>
+                    <div className='boxes'>
                         <h2 className='waveAnimationSlow'>Currently working on...</h2>
-                        <i className="fas fa-pen-fancy"></i>
-                        <p>I am working on web development to show you guys <br /> my projects that I done. <br/> ;)</p>
+                        <div className='box-text'>
+                            <i className="fas fa-pen-fancy"></i>
+                            <p>I am working on web development to show you guys <br /> my projects that I done. <br/> ;)</p>
+                        </div>
                     </div>
                 </div>
             </div>
