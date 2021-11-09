ab168f9b2d8b4b5a11c2020279fe703b5e5d2c41
L-Limbu <007limbu@gmail.com>
2021-11-09 17:49:35 +0000

    imported native haptics

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 import '../App.css';
 import { useHistory } from 'react-router-dom';
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+
 
 export default function HomePage() {
     const history = useHistory()
+    function handleDisplay() {
+      const options = {
+        enableVibrateFallback: true,
+        ignoreAndroidSystemSettings: false
+      }
+      return ReactNativeHapticFeedback.trigger('impactMedium', options);
+    }
     return (
         
         <div>
@@ -13,7 +22,7 @@ export default function HomePage() {
             <p className='waveAnimationSlow homePage-text'>
               Welcome to my portfolio website. <br/>Hope you enjoy my work :)
             </p>
-            <button className='home-button' onClick={() => history.push('/display')}> 
+            <button className='home-button' onClick={() => history.push('/display')} onPress={handleDisplay}> 
               Display
             </button>
     
