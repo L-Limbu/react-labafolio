e115be35ed37c4daff65e0e130bcef62d8a0e6e4
L-Limbu <007limbu@gmail.com>
2022-01-04 15:40:43 +0000

    import resources for the page

--- src/pages/uniProjects/atom-trap/atomTrap.js
+++ src/pages/uniProjects/atom-trap/atomTrap.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import './atomTrap.css'
+
+import image1 from './resources/AtomTrap.png'
+import image2 from './resources/AtomTrapXZ.png'
+import image3 from './resources/AtomTrap3D.png'
+import video1 from './resources/penningTrapXY.mov'
+import video2 from './resources/penningTrapXZ.mm4'
+import video3 from './resources/penningTrap3d.mm4'
+
+
 
 
 export default function AtomTrap () {
