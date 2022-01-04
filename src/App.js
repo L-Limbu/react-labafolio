cd12a9cb5558f94b675ab82e8e8c2775c8d51ee7
L-Limbu <007limbu@gmail.com>
2022-01-04 16:07:51 +0000

    connecting atom trap page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PersonalPage from './pages/personalProject/personalProject';
 import Display from './pages/display';
 import Workpage from './pages/Works/workPage';
 import SolarSystem from './pages/uniProjects/solar-system/solarSystem';
-import AtomTrap from './pages/uniProjects/atomTrap';
+import AtomTrap from './pages/uniProjects/atom-trap/atomTrap';
 import IsingModel from './pages/uniProjects/isingModel';
 import Spectroscopy from './pages/uniProjects/spectroscopy';
 function App() {
