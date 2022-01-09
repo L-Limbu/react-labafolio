d1b6a98f50bde57dde6ee67c5f67a8041c84df4b
L-Limbu <007limbu@gmail.com>
2022-01-09 14:17:12 +0000

    added the body to astro page

---
+++ src/pages/uniProjects/astro-photo/astroPics.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './astroPics.css'
+
+import image1 from './resources/M7.png'
+import image2 from './resources/M4.png'
+import image3 from './resources/M33.png'
+import image4 from './resources/Helix-nebula.png'
+
+export default function AstroPhotograph () {
+    return (
+        <div className='astro-body '>
+            <h1 className='waveAnimationSlow'> Astrophotography</h1>
+            <div className='text-wrapper'>
+                <h2 className='waveAnimationSlow' className='astro-h2'>Exploring the Universe</h2>
+
+            </div>
+        </div>
+    )
+}
