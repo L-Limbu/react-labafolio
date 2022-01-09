52a8173c1a02e78324bfc7948493655c75984344
L-Limbu <007limbu@gmail.com>
2022-01-09 14:34:59 +0000

    installed package on locally

---
+++ src/pages/uniProjects/astro-photo/resources/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function SlideShow() {
+    return (
+        <section className='slideShow'>
+            Wonders of Universe
+        </section>
+    )
+}
